d761e946872710f7d14ee3f7bba8d801117cbbde
Joel Clark <jc214809@ohio.edu>
2015-08-17 14:11:12 -0400

    added my team

--- js/Scoreboard2.js
+++ js/Scoreboard2.js
@@ -73,7 +73,13 @@ myApp.controller('baseballController', function($scope, $http, $q, $timeout) {
         $scope.whichTeam = team;
         var theSelectedDate = parseDate(selectedDate.getFullYear() + '-' + (selectedDate.getMonth() + 1) + '-' + selectedDate.getDate());
         if ($scope.whichTeam == "Mine") {
-            if (theSelectedDate.between(parseDate('2015-08-10'), parseDate('2015-08-16'))) {
+            if (theSelectedDate.between(parseDate('2015-08-17'), parseDate('2015-08-23'))) {
+                //alert("Playoff Push");
+                $scope.myTeam = ['457763','547989','543829','570256','592518','435622','285078','425783','471865'];
+                $scope.benchPlayers = ['434670', '457759', '630111', '425877', '475582', '592626'];
+                $scope.DLPlayers =['431151']
+                $scope.myPitchingStaff = 'lan';
+            } else if (theSelectedDate.between(parseDate('2015-08-10'), parseDate('2015-08-16'))) {
                 //alert("I need a win!!!");
                 $scope.myTeam = ['457763','547989','543829','475582','592518','435622','285078','425783','471865'];
                 $scope.benchPlayers = ['434670', '425567', '630111', '425877', '570256', '592626'];
